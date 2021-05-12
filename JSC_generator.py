f2aefecd3bc8e36362959398538ffd1f47d677a6
yzl1416750218 <62953057+yzl1416750218@users.noreply.github.com>
2021-05-12 15:30:47 +0800

    Update JSC_generator.py

--- JSC_generator.py
+++ JSC_generator.py
@@ -5,6 +5,8 @@ import xlwt
 from data_1 import sno_data
 from data_2_2 import  c_id_data
 
+jsc_test = []
+jsc_test_temp =()
 c_id_data_temp = ['CS-01','CS-02','CS-04','EE-01','EE-02']
 c_id_data.extend(c_id_data_temp)
 f = Faker(locale='zh_cn')
@@ -20,6 +22,12 @@ while n <= 5000:
     n += 1
     sno = ''.join(random.choice(sno_data))
     c_id = ''.join(random.choice(c_id_data))
+    jsc_test_temp = (sno,c_id)
+    while jsc_test_temp in jsc_test:
+        sno = ''.join(random.choice(sno_data))
+        c_id = ''.join(random.choice(c_id_data))
+        jsc_test_temp = (sno, c_id)
+    jsc_test.append(jsc_test_temp)
     grade = f.pyint(min_value=1, max_value=100, step=1)
     shet.write(n, 0, sno)  # 第一个参数表示行，第二个表示列,第三个自己需要的数据
     shet.write(n, 1, c_id)
